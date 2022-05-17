a2f0612619f36d2679dc4742ecd1a27bd03a1e95
venuatu <venuatu@gmail.com>
2022-05-18 00:11:21 +1000

    swp/kalecgos: add health bars to Kal Frame

--- DBM-Sunwell/Kalecgos.lua
+++ DBM-Sunwell/Kalecgos.lua
@@ -14,6 +14,7 @@ mod:RegisterEventsInCombat(
 	"SPELL_CAST_SUCCESS 45018",
 	"SPELL_AURA_APPLIED 44978 45001 45002 45004 45006 45010 45029 46021 45018",
 	"SPELL_AURA_APPLIED_DOSE 45018",
+	"UNIT_HEALTH mouseover target",
 	"UNIT_DIED"
 )
 
@@ -145,5 +146,19 @@ function mod:UNIT_DIED(args)
 			grp = 0
 		end
 		Kal:RemoveEntry(("%s (%d)"):format(args.destName, grp or 0))
+	end
+end
+
+function mod:UNIT_HEALTH(uId)
+	local id = self:GetUnitCreatureId(uId)
+	if id == 24850 or id == 24892 then
+		self:SendSync("Health", id, math.floor(UnitHealth(uId) / UnitHealthMax(uId)))
+	end
+end
+
+function mod:OnSync(msg, id, health, sender)
+	if msg ~= "Health" then return end
+	if self.Options.ShowFrame then
+		Kal:UpdateHealth(id, health)
 	end
 end
